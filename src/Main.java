75d44018a4523f8566cdc32ca68a1971c1a2e47a
Людок <skobeleva.ludmila@yandex.ru>
2022-11-24 12:06:20 +0300

    часть задания

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,12 @@ public class Main {
 
         Book titel= new Book ("Три мушкетера ", "Собачье сердце", 1844, 1925);
         titel.year=1845;
-        System.out.println(titel.getName()+ " "+ titel.getYear());
         titel.year2=1922;
-        System.out.println(titel.getName2()+ " "+ titel.getYear2());
 
         Author name= new Author("Дюма", "Булгаков", "Александр", "Михаил");
-        System.out.println(name.getName() + " "+ name.getSurname());
-        System.out.println(name.getName2()+ " " + name.getSurname2());
+
+        System.out.println(titel.getName()+ " "+ titel.getYear()+" "+name.getName() + " "+ name.getSurname());
+        System.out.println(titel.getName2()+ " "+ titel.getYear2()+" "+name.getName2()+ " " + name.getSurname2());
 
 
     }
